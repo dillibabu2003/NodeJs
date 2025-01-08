3a0286a93fa7f5d1e8be4057844820faa61f40d2
Dilli Babu R <dillibabu12214@gmail.com>
2025-01-08 10:47:00 +0530

    mogodb code

---
+++ database.js
@@ -0,0 +1,20 @@
+const {MongoClient}=require("mongodb");
+
+const URI="mongodb+srv://dillibabu_R:mongodbaccessgranted@cluster0.xjysbg6.mongodb.net/"
+
+const client=new MongoClient(URI);
+const dbName="NodeTutorial";
+
+async function main(){
+        await client.connect();
+        console.log("Connected Successfully to server");
+        const db=client.db(dbName);
+        const collection=db.collection('user');
+        const result=await collection.find({}).toArray();
+        console.log(result);
+        return 'completed';
+}
+main().
+then(console.log)
+.catch(console.error)
+.finally(()=>client.close())
